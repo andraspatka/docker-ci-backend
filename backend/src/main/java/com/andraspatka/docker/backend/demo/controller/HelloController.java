0374da5a9c6c8db46a126a3de9f14c0c86ef2762
Patka Zsolt-Andras <andras.patka@gmail.com>
2020-03-15 01:37:06 +0200

    Added cors support

--- backend/src/main/java/com/andraspatka/docker/backend/demo/controller/HelloController.java
+++ backend/src/main/java/com/andraspatka/docker/backend/demo/controller/HelloController.java
@@ -5,10 +5,7 @@ import com.andraspatka.docker.backend.demo.model.HelloRequest;
 import com.andraspatka.docker.backend.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -21,11 +18,13 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @CrossOrigin(origins = {"localhost:80", "localhost:3000"})
     @PutMapping(value = "/greeting", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public Hello putGreeting(@RequestBody @Nullable HelloRequest request) {
         return this.helloService.getHelloMessage(request);
     }
 
+    @CrossOrigin(origins = {"localhost:80", "localhost:3000"})
     @GetMapping(value = "/greeting", produces = APPLICATION_JSON_VALUE)
     public Hello getGreeting(@RequestBody @Nullable HelloRequest request) {
         return this.helloService.getHelloMessage(request);
