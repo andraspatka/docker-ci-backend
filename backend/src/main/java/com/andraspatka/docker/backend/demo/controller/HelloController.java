f30932f3ff9827ac19e5192d9ec5750e0ee54c47
Patka Zsolt-Andras <andras.patka@gmail.com>
2020-03-14 23:12:19 +0200

    Added put http method for getGreeting, so frontend can send payload

--- backend/src/main/java/com/andraspatka/docker/backend/demo/controller/HelloController.java
+++ backend/src/main/java/com/andraspatka/docker/backend/demo/controller/HelloController.java
@@ -6,6 +6,7 @@ import com.andraspatka.docker.backend.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,11 @@ public class HelloController {
 
     public HelloController(@Autowired HelloService helloService) {
         this.helloService = helloService;
+    }
+
+    @PutMapping(value = "/greeting", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public Hello putGreeting(@RequestBody @Nullable HelloRequest request) {
+        return this.helloService.getHelloMessage(request);
     }
 
     @GetMapping(value = "/greeting", produces = APPLICATION_JSON_VALUE)
